0bb5885e1a1516a7de4c274b43427b42251949db
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-05-01 14:10:11 -0400

    Completed

--- Exercise_2.js
+++ Exercise_2.js
@@ -1,32 +1,98 @@
-class StackAsLinkedList {
-​
-    static stackNode = class {
-​
-        constructor(d) {
-            //Constructor here
-            this.data = d;
-            this.next = null;
-        }
-    }
-​
-    function isEmpty() {
-        //Write your code here for the condition if stack is empty.
-    }
-​
-    function push(data) {
-       //Write code to push data to the stack.
-    }
-​
-    function pop() {
-       //If Stack Empty Return 0 and print "Stack Underflow"
-       //Write code to pop the topmost element of stack.
-       //Also return the popped element
-    }
-​
-    function peek() {
-       //Write code to just return the topmost element without removing it.
+// Any problem you faced while coding this : Yes, Missed on few edge cases when dealing with POP 
+
+class Node {
+    constructor(d) {
+        //constructor is called everytime the object of this class is created
+        this.data = d // Will store the data
+        this.next = null // will store pointing to another node
     }
 }
+class StackAsLinkedList {
+    constructor(d) {
+        this.head = null //initialy every linked list has null head
+    }
+
+    isEmpty(){
+        // Time Complexity :  O (1) 
+        // Space Complexity : O(1)
+
+        //Check if there is no head 
+        if(this.head){
+            //This block will execute if head is present
+            //that means linked list is not empty
+            return false
+        }
+        else{
+            // this block will run if there is no head that means LL is empty
+            return true
+        }
+
+    }
+    push(data) {
+        // Time Complexity :  O (n) 
+        // Space Complexity : O(1)
+
+        //Write code to push data to the stack.
+        //Condition 1 : if making head when LL is empty
+        // Condition 2: if adding it to the last position
+        if(!this.head){
+            //no head present that means make new node turn it into head
+            this.head = new Node(data,null)
+        }
+        else{
+            let current = this.head
+            while(current.next){
+                //traverse the last element
+                current = current.next
+            }
+            //This block will execute when the current node is last node and is pointing to null
+            current.next = new Node(data,null)
+        }
+    }
+    pop() {
+
+        // Time Complexity :  O (n) 
+        // Space Complexity : O(1)
+
+        //If Stack Empty Return 0 and print "Stack Underflow"
+
+        if (!this.head) {
+            console.log("Stack Underflow")
+            return 0
+        }
+        if(!this.head.next){
+            //this means this operation is asking us to remove the only ele(head) from the LL
+            let ele = this.head.data
+            this.head = null
+            return ele
+        }
+        //Write code to pop the topmost element of stack.
+        //Traverse to the current.next.next == null
+        let current = this.head
+        while(current.next.next){
+            current = current.next
+        }
+        //this means we have reached the poistion where we are pointing the second last node
+        let ele = current.next.data
+        current.next = null
+        return ele
+        // the second last element should point to null resulting in deletion of element as we lose the pointer to last node
+        //Also return the popped element
+    }
+    peek() {
+        // Time Complexity :  O (n) 
+        // Space Complexity : O(1)
+        //Write code to just return the topmost element without removing it.
+        //Travel to the last element and returns its data
+        let current = this.head
+        while (current.next) {
+            current = current.next
+        }
+        //this means we are at the last ele
+        return current.data
+    }
+}
+
 //Driver code
 const sll = new StackAsLinkedList();
 sll.push(10);
--- Exercise_3.js
+++ Exercise_3.js
@@ -1,44 +1,53 @@
-// Java program to implement
-// a Singly Linked List
+//Any problem you faced while coding this : Had to change the initial boilerplate code to set to current stadndards of writing DSA implentation.
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
 class LinkedList {
     constructor() {
         this.head = null;
     }
-     // Linked list Node.
-     static Node = class {
-        constructor(d) {
-            this.data = d;
-            this.next = null;
+
+    insert(data) {
+        // Time Complexity :  O (n) 
+        // Space Complexity : O(1)
+        //condition 1: list is empty make the new data the new head
+        //If we dont have head then create a new node and make it head
+        if (!this.head) {
+            this.head = new Node(data);
+            return;
         }
+        //condition 2: we already have a head just append to the last position
+        //This block will execute for 2nd condi
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = new Node(data);
     }
-​
-     // Method to insert a new node
-    function insert(list, data) {
-        // Create a new node with given data
-​
-        // If the Linked List is empty,
-        // then make the new node as head
-​
-        // Else traverse till the last node
-        // and insert the new_node there
-​
-        // Insert the new_node at last node
-        // Return the list by head
-    }
-​
-     // Method to print the LinkedList.
-    function printList(list) {
-        // Traverse through the LinkedList
-​
-        // Print the data at current node
-​
-        // Go to next node
+
+    printList() {
+        // Time Complexity :  O (n) 
+        // Space Complexity : O(1)
+        //Use current as temp node to travel the linkedList
+        let current = this.head;
+        let elements = [];
+        //untill we find the null keep pushing it to the array
+        //move current pointer to the next node
+        while (current) {
+            elements.push(current.data);
+            current = current.next;
+        }
+        console.log(elements);
+
     }
 }
        // Driver code
        /* Start with the empty list. */
        let list = new LinkedList();
-​
         // ******INSERTION******
         // Insert the values
         list.insert(list, 1);
